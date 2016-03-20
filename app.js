b6036c5bcb963b8ae8150556ad8de7ddb5617cef
chertpong_h <chertpong_h@cmu.ac.th>
2016-03-20 18:10:52 +0700

    Add boot script (auto migrate data)

--- app.js
+++ app.js
@@ -28,8 +28,11 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', routes);
+//set boot script
+require('./boot')();
 
+// set route
+app.use('/', routes);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ boot/course-init.js
@@ -0,0 +1,29 @@
+var Course = require('./../models/course');
+
+module.exports = function(){
+    Course
+        .remove({})
+        .then(function(data){
+            console.log('[-]' + 'Course ' + (data.result.ok ? 'success' : 'fail') );
+            Course
+                .create({
+                    name:'Java OOP',
+                    duration:10,
+                    price:1000,
+                    description:'Best java basic course',
+                    thumbnail:'https://upload.wikimedia.org/wikipedia/en/8/88/Java_logo.png',
+                    tags:[
+                        'Java','Beginner'
+                    ]}
+                )
+                .then(function(data){
+                    console.log('[+] Course: '+data.name);
+                })
+                .catch(function(err){
+                    throw err;
+                });
+        })
+        .catch(function(err){
+            throw err;
+        });
+};
